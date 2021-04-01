fd7f206fe317f1d8ca5e4ac46c5b1373865401c1
sourcegilmar <gilmardw@gmail.com>
2021-04-01 15:03:01 -0300

    Implementacao classe principal

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,43 @@
 package aplicacao;
 
+import javax.swing.JOptionPane;
+
+import entidades.Honda;
+import entidades.Suzuki;
+import entidades.Yamaha;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		while (true) {
+			try {
+				String nome = JOptionPane
+						.showInputDialog("Informe a marca [Cancelar] para sair:  ");
+				if (nome.equalsIgnoreCase("Honda")) {
+					Honda moto = new Honda();
+					JOptionPane.showMessageDialog(null, "\nNome: " + moto.nome
+							+ "\nCilindrada: " + moto.cilindrada + "\nCor: "
+							+ moto.cor, "Dados da moto",
+							JOptionPane.CLOSED_OPTION);
+				} else if (nome.equalsIgnoreCase("Yamaha")) {
+					Yamaha moto = new Yamaha();
+					JOptionPane.showMessageDialog(null, "\nNome: " + moto.nome
+							+ "\nCilindrada: " + moto.cilindrada + "\nCor: "
+							+ moto.cor, "Dados da moto",
+							JOptionPane.CLOSED_OPTION);
+				} else if (nome.equalsIgnoreCase("Suzuki")) {
+					Suzuki moto = new Suzuki();
+					JOptionPane.showMessageDialog(null, "\nNome: " + moto.nome
+							+ "\nCilindrada: " + moto.cilindrada + "\nCor: "
+							+ moto.cor, "Dados da moto",
+							JOptionPane.CLOSED_OPTION);
+				}
+			} catch (Exception e) {
+				break;
+			}
+		}
+		System.out.println("\nPROGRAMA FINALIZADO!");
 
 	}
 
