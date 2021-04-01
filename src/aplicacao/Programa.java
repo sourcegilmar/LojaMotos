1896cdda24f6ecb22eeff685d0808d8528896594
sourcegilmar <gilmardw@gmail.com>
2021-04-01 15:17:58 -0300

    2a Refatoracao - Alteracao para usar a abstracao - Moto

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -22,13 +22,13 @@ public class Programa {
 							+ moto.cor, "Dados da moto",
 							JOptionPane.CLOSED_OPTION);
 				} else if (nome.equalsIgnoreCase("Yamaha")) {
-					Yamaha moto = new Yamaha();
+					Moto moto = new Yamaha();
 					JOptionPane.showMessageDialog(null, "\nNome: " + moto.nome
 							+ "\nCilindrada: " + moto.cilindrada + "\nCor: "
 							+ moto.cor, "Dados da moto",
 							JOptionPane.CLOSED_OPTION);
 				} else if (nome.equalsIgnoreCase("Suzuki")) {
-					Suzuki moto = new Suzuki();
+					Moto moto = new Suzuki();
 					JOptionPane.showMessageDialog(null, "\nNome: " + moto.nome
 							+ "\nCilindrada: " + moto.cilindrada + "\nCor: "
 							+ moto.cor, "Dados da moto",
